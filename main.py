ba5f62a5edb40d5a2ab857e92235483a7f361c63
juanrmv528e <juanramboy1@gmail.com>
2026-04-27 23:15:41 -0500

    11

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-"""Laboratorio 8 - CLI del gestor de tareas."""
-
-# TODO: Implementar CLI según README.md
-
 import sys
 from todo_manager import read_todo_file, write_todo_file
  
 try:
+    
     if len(sys.argv) < 2:
         raise IndexError("Insufficient arguments provided!")
  
     file_path = sys.argv[1]
  
-
+    
     if file_path == "--help":
         print("""Usage: python main.py <file_path> <command> [arguments]...
  
@@ -26,27 +23,29 @@ Examples:
   python main.py tasks.txt view
   python main.py tasks.txt add "Call mom" remove "Take out trash" view""")
         sys.exit(0)
-
+ 
+    
     tasks = read_todo_file(file_path)
  
+   
     if len(sys.argv) < 3:
         sys.exit(0)
  
-
+    
     args = sys.argv[2:]  
     i = 0
     modified = False
  
     while i < len(args):
         command = args[i]
-    
-    if command == "view":
+ 
+        if command == "view":
             print("Tasks:")
             for task in tasks:
                 print(task)
             i += 1
  
-    elif command == "add":
+        elif command == "add":
             if i + 1 >= len(args):
                 raise IndexError('Task description required for "add".')
             task_desc = args[i + 1]
@@ -54,8 +53,8 @@ Examples:
             print(f'Task "{task_desc}" added.')
             modified = True
             i += 2
-    
-    elif command == "remove":
+ 
+        elif command == "remove":
             if i + 1 >= len(args):
                 raise IndexError('Task description required for "remove".')
             task_desc = args[i + 1]
@@ -69,6 +68,7 @@ Examples:
  
         else:
             raise ValueError("Command not found!")
+ 
     
     if modified:
         write_todo_file(file_path, tasks)
